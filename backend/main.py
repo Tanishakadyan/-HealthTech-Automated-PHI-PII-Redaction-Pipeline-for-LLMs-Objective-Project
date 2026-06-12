5bfd02c4793e820028eb2b372afb4012daacc4c8
Sathish Munneswaran s <ssathish69930@gmail.com>
2026-06-12 11:10:28 +0530

    Add redact endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Backend Working"}
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/redact")
+def redact(data: TextInput):
+    return {
+        "status": "success",
+        "received_text": data.text
+    }
